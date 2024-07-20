d68ad866913b23389ebc80ece9af44f300947182
Nikhil Singh <135117065+nikhilsingh7777@users.noreply.github.com>
2024-07-20 13:44:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
 const carRoute = require('./routes/car.route.js');
-
 require("dotenv").config();
 
 const app = express();
@@ -14,8 +12,9 @@ app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 }).then(() => {
     console.log("MongoDB connection established");
 }).catch((err) => console.log("MongoDB connection failed: ", err.message));
