ab95ae58834d6d624c14b5644cca740e608fb091
Nikhil Singh <135117065+nikhilsingh7777@users.noreply.github.com>
2024-07-20 12:42:17 +0530

    Update car.route.js

--- routes/car.route.js
+++ routes/car.route.js
@@ -1,44 +1,36 @@
 const express = require('express');
-const Car = require('../models/carModel.js')
+const Car = require('../models/carModel.js');
 
 const router = express.Router();
 
+// Route to register a new car
 router.post("/register", async (req, res) => {
-
     try {
-
-        const newCar = new Car({
-            ...req.body,
-        });
-
+        const newCar = new Car(req.body);
         await newCar.save();
         res.status(201).send("Car has been added");
-
     } catch (error) {
-        console.log(error);
+        console.error(error);
+        res.status(500).send("Error adding car");
     }
 });
 
-
-
+// Route to get all cars
 router.get("/", async (req, res) => {
-
     try {
         const cars = await Car.find({});
-
         res.status(200).json(cars);
     } catch (error) {
-        console.log(error);
+        console.error(error);
+        res.status(500).send("Error fetching cars");
     }
-})
+});
 
+// Route to delete a car by id
 router.delete("/:id", async (req, res) => {
     const { id } = req.params;
 
-    console.log(id);
-
     try {
-
         const deletedCar = await Car.findByIdAndDelete(id);
 
         if (!deletedCar) {
@@ -46,12 +38,10 @@ router.delete("/:id", async (req, res) => {
         }
 
         res.status(200).send("Car has been deleted");
-
     } catch (error) {
-        console.log(error);
+        console.error(error);
         res.status(500).send("Error deleting car");
     }
 });
 
-
-module.exports = router
+module.exports = router;
