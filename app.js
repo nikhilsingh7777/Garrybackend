ae26a2ddb76c28837d506801ce3433fab226ecd7
Nikhil Singh <135117065+nikhilsingh7777@users.noreply.github.com>
2024-07-20 12:44:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,28 +2,30 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const carRoute = require('./routes/car.route.js')
+const carRoute = require('./routes/car.route.js');
 
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({origin: "http://localhost:3000", credentials: true}));
-const PORT = process.env.PORT;
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI, {
-    {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}
 }).then(() => {
     console.log("MongoDB connection established");
 }).catch((err) => console.log("MongoDB connection failed: ", err.message));
 
 app.get('/', (req, res) => {
-  res.send('All set!');
+    res.send('All set!');
 });
+
 app.use("/api/car", carRoute);
-app.listen(PORT, (req, res) => {
+
+app.listen(PORT, () => {
     console.log(`Server running on PORT: ${PORT}`);
-})
+});
