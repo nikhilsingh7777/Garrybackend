b20c1e7e23a95d1c6617c127e255159e9680cd53
Nikhil Singh <135117065+nikhilsingh7777@users.noreply.github.com>
2024-07-20 11:32:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ mongoose.connect(process.env.MONGO_URI, {}).then(() => {
     console.log("MongoDB connection established");
 }).catch((err) => console.log("MongoDB connection failed: ", err.message));
 
-
+app.get('/', (req, res) => {
+  res.send('All set!');
+});
 app.use("/api/car", carRoute);
 
 app.listen(PORT, (req, res) => {
